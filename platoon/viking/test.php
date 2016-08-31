e86d9c4661f272714ffa699f50dcf8526865eaf2
Ryahn <ryancarr10@gmail.com>
2016-08-31 12:33:22 -0400

    Working on award page. Not finished

--- platoon/viking/test.php
+++ platoon/viking/test.php
@@ -1,6 +1,130 @@
- <?php
- $a1=26;
-$b1=40;
-for($i=$a1; $i<=$b1; $i++) {
-	echo $i;
-}
+<?php
+include('../../config/protection.php');
+include('../../templates/head.php');
+include('../../templates/nav.php');
+
+
+?>
+<div id="wrapper">
+	<div id="page-wrapper">
+    	<div class="row">
+	        <div class="col-lg-12">
+	            <h1 class="page-header">Chain of Command</h1>
+	        </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+
+<div class="col-md-5"></div>
+<div class="col-md-2" style="color: #c7c7c7;text-align: center;
+    margin-top: 20px;
+    background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Officer in Command</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/officer/2LT.png" /><span style="font-size:16px"> Larig</span>
+</div>
+<div class="col-md-5"></div>
+</div>
+
+<div class="row">
+<div class="col-md-5"></div>
+<div class="col-md-2 top-bar" style="color: #c7c7c7;text-align:center;margin-top:20px;    background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Platoon Sergeant</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/SGM.png" /><span style="font-size:16px">Shepard</span>
+</div>
+<div class="col-md-5"></div>
+</div>
+
+<div class="row">
+
+<div class="col-md-5"></div>
+
+<div class="col-md-2 top-bar bottom-bar1" style="color: #c7c7c7;text-align:center;margin-top:20px;    background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Operations Sergeant</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/1SG.png" /><span style="font-size:16px" class="bottom-split-bar">Pred</span>
+</div>
+
+<div class="col-md-5"></div>
+</div>
+
+<div class="row">
+	<div class="col-md-1" style="width:12%;"></div>
+	<div class="col-md-2 bottom-bar2" style="color: #c7c7c7;text-align:center;margin-top:25px;background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Master Sergeant</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/MSG.png" /><span class="bottom-split-bar1" style="font-size:16px">Ejmonkey</span>
+</div>
+
+<div class="col-md-5" style="margin-left:3px;"></div>
+
+<div class="col-md-2 bottom-bar2" style="color: #c7c7c7;text-align:center;margin-top:25px;background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;    padding-bottom: 22px;"><span style="font-size:20px">Master Sergeant</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/MSG.png" /><span class="bottom-split-bar2" style="font-size:16px"></span>
+</div>
+
+
+
+</div>
+<div class="row">
+	<div class="col-md-1" style="color: #c7c7c7;text-align:center;margin-top:25px;background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;    margin-left: -120px;"><span style="font-size:20px">Sergeant First Class</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/MSG.png" /><span style="font-size:16px">Chappin</span>
+</div>
+
+<div class="col-xs-4" style="margin-left:-44px"></div>
+
+
+<div class="col-md-1" style="color: #c7c7c7;text-align:center;margin-top:25px;background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Sergeant First Class</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/MSG.png" /><span style="font-size:16px">Frosty</span>
+</div>
+
+
+<div class="row">
+	
+<div class="col-xs-2" style="margin-left: -35px;"></div>
+<div class="col-md-1" style="color: #c7c7c7;text-align:center;margin-top:25px;background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Sergeant First Class</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/MSG.png" /><span style="font-size:16px">Frerking</span>
+</div>
+<div class="col-xs-2" style="margin-left:-10px"></div>
+<div class="col-md-1" style="color: #c7c7c7;text-align:center;margin-top:25px;background-color: rgba(27, 27, 27, 0.92);
+    border-radius: 10px;"><span style="font-size:20px">Sergeant First Class</span><br />
+<img style="display: block;
+    margin-left: auto;
+    margin-right: auto" height="40px" src="/assets/images/ranks/enlisted/MSG.png" /><span style="font-size:16px">Hearts</span>
+</div>
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+<?php
+include('../../templates/footer.php');
+?>
